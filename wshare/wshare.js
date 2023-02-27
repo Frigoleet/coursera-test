181deed3f404cb5d2f1840c50d4d2af1d09efa43
Frigoleet <jesper.goethe@gmail.com>
2023-02-27 23:08:00 +0100

    big update to buttons

--- wshare/wshare.js
+++ wshare/wshare.js
@@ -1,9 +1,16 @@
 
 
 function openFile() {
-    const fileUrl = "../media/Jesper-Göthe.vcf";
+    const fileUrl = "../media/Jesper-Göthe.vcf"; //revise
     window.open(fileUrl, '_blank');
   }
+
   function openUrl(url) {
     window.open(url, '_blank');
   }
+import { library, icon } from '@fortawesome/fontawesome-svg-core'
+import { faCamera } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCamera)
+
+const camera = icon({ prefix: 'fas', iconName: 'camera' })
