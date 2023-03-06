175a9a80294a1439a44b513a3df6b563faf006ad
Frigoleet <jesper.goethe@gmail.com>
2023-03-06 22:20:39 +0100

    made buttons clickable and increased text size

--- wshare/wshare.js
+++ wshare/wshare.js
@@ -1,16 +1,8 @@
-
-
 function openFile() {
     const fileUrl = "../media/Jesper-Göthe.vcf"; //revise
     window.open(fileUrl, '_blank');
   }
-
-  function openUrl(url) {
-    window.open(url, '_blank');
-  }
-import { library, icon } from '@fortawesome/fontawesome-svg-core'
-import { faCamera } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faCamera)
-
-const camera = icon({ prefix: 'fas', iconName: 'camera' })
+  
+function openUrl(url) {
+  window.open(url, '_blank');
+}
